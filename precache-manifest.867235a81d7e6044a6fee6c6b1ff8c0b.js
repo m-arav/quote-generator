bf7a27672dc44727c9193b2da6e774f81f78502b
Aravind <thearavindmurali@gmail.com>
2020-08-07 07:19:10 +0530

    Updates

---
+++ precache-manifest.867235a81d7e6044a6fee6c6b1ff8c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9047f536f702be4b87f00918cbe37c",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "6506b625cb1cef2914d2",
+    "url": "/quote-generator/static/css/main.647dfd6f.chunk.css"
+  },
+  {
+    "revision": "78d7c3098c5822609c5a",
+    "url": "/quote-generator/static/js/2.e735cf91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.e735cf91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6506b625cb1cef2914d2",
+    "url": "/quote-generator/static/js/main.0997a594.chunk.js"
+  },
+  {
+    "revision": "f39db142146ffeee31ea",
+    "url": "/quote-generator/static/js/runtime-main.2369a586.js"
+  }
+]);
